c874628d0ffd52c16929774c8f75e978a1a2aed8
Peter Siman <shimon001@gmail.com>
2022-12-08 21:20:27 +0100

    WIP - basic list of events + contact form

--- pages/akce/index.jsx
+++ pages/akce/index.jsx
@@ -1,7 +1,69 @@
+import EventThumb from '/public/images/placeholders/EventItem.jpg'
+import stylesEvents from '/components/events/events.module.scss'
+import { EventListItem } from '/components/events'
+import { PrimaryButton } from '/components/Buttons'
+
+
 const Events = () => {
     return (
-        <div className="container">
-            <h1>Akce</h1>
+        <div className="container mt-5">
+            <div className={stylesEvents.event_list__description}>
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1>Spolek Narativ pořádá:</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <EventListItem
+                        id="1"
+                        title="Úvod do teorie a praxe Otevřeného dialogu"
+                        description={(
+                            <p>Termín: 3.-4. 3. 2022 (vždy 9:00 – 16:30),<br />
+                            Rozsah: 16 výukových hodin; Cena: 3.200,- Kč</p>
+                        )}
+                        image={EventThumb}
+                    />
+                </div>
+                <div className="col-md-6">
+                    <EventListItem
+                        id="2"
+                        title="Úvod do teorie a praxe Otevřeného dialogu"
+                        description={(
+                            <p>Termín: 3.-4. 3. 2022 (vždy 9:00 – 16:30)</p>
+                        )}
+                        image={EventThumb}
+                    />
+                </div>
+                <div className="col-md-6">
+                    <EventListItem
+                        id="3"
+                        title="Úvod do teorie a praxe Otevřeného dialogu"
+                        description={(
+                            <p>Termín: 3.-4. 3. 2022 (vždy 9:00 – 16:30),<br />
+                                Rozsah: 16 výukových hodin; Cena: 3.200,- Kč</p>
+                        )}
+                        image={EventThumb}
+                    />
+                </div>
+                <div className="col-md-6">
+                    <EventListItem
+                        id="4"
+                        title="Úvod do teorie a praxe Otevřeného dialogu"
+                        description={(
+                            <p>Termín: 3.-4. 3. 2022 (vždy 9:00 – 16:30),<br />
+                                Rozsah: 16 výukových hodin; Cena: 3.200,- Kč</p>
+                        )}
+                        image={EventThumb}
+                    />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-md-12 text-center'>
+                    <PrimaryButton>Zobrazit další akce</PrimaryButton>
+                </div>
+            </div>
         </div>
     )
 }
--- pages/kontakt.jsx
+++ pages/kontakt.jsx
@@ -1,7 +1,36 @@
 const Contact = () => {
     return (
-        <div className="container">
-            <h1>Kontakt</h1>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6">
+                    <h1>Kontakt</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h1>Napiste nam:</h1>
+                    <form>
+                        <div className="form-group">
+                            <label for="inputName">Your name</label>
+                            <input type="name" className="form-control" id="inputName" aria-describedby="nameHelp" placeholder="Your name here"/>
+                            <small id="nameHelp" className="form-text text-muted">We'll never share your name with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputEmail">Your email</label>
+                            <input type="email" className="form-control" id="inputEmail" placeholder="Your email here"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputSubject">Subject</label>
+                            <input type="email" className="form-control" id="inputSubject" placeholder="Subject"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="textareaMessage">Message</label>
+                            <textarea class="form-control" id="textareaMessage" rows="3" placeholder="Your message here"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
