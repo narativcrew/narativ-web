57694b53327a2f69b1aa1b6aaa79a0799c63cb5d
Peter Siman <shimon001@gmail.com>
2023-04-27 16:53:53 +0200

    Fix excerpt

--- components/members/MemberListItem.jsx
+++ components/members/MemberListItem.jsx
@@ -3,9 +3,20 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { CardButton } from 'components/Buttons';
 import cx from 'classnames';
-import { PrismicRichText } from '@prismicio/react';
+import * as prismicH from '@prismicio/helpers';
 
 import styles from './members.module.css';
+
+const getExcerpt = (textField) => {
+    const text = prismicH.asText(textField);
+
+    const excerpt = text.substring(0, 300);
+
+    if (text.length > 300) {
+        return `${excerpt.substring(0, excerpt.lastIndexOf(' '))}…`;
+    }
+    return excerpt;
+};
 
 const MemberListItem = ({ id, name, desc, image }) => (
     <div className="col-lg-4 mb-4">
@@ -13,9 +24,7 @@ const MemberListItem = ({ id, name, desc, image }) => (
             <div className={styles.memberCardImg} style={{ backgroundImage: `url(${image})` }} />
             <div className={cx('text-start', styles.memberCardContent)}>
                 <h5 className={styles.memberCardTitle}>{name}</h5>
-                <div className={styles.memberCardText}>
-                    <PrismicRichText field={desc} />
-                </div>
+                <div className={styles.memberCardText}>{getExcerpt(desc)}</div>
                 <div className={styles.memberCardButtonWrapper}>
                     <Link href={`/clenove/${id}`}>
                         <CardButton>Vice</CardButton>
