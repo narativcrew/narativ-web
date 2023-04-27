ef68d0b217951813f3670e87beacfc19ba035c94
Peter Siman <shimon001@gmail.com>
2023-04-27 11:44:03 +0200

    Integration with SendGrid for sending emails

--- pages/kontakt.jsx
+++ pages/kontakt.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
@@ -7,75 +7,190 @@ import FeaturedImage from 'components/FeaturedImage';
 import { BlockButton } from 'components/Buttons';
 import { createClient } from 'prismicio';
 
-const Contact = ({ headerImage }) => (
-    <>
-        <Head>
-            <title>Narativ | Kontakt</title>
-            <meta property="og:title" content="Narativ | Kontakt" key="title" />
-        </Head>
+const Contact = ({ headerImage }) => {
+    // States for contact form fields
+    const [fullname, setFullname] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
 
-        <FeaturedImage image={headerImage.data.image.url} />
-        <div className={stylesContact.contactBgBox}>
-            <div className="container pb-5 pt-2">
-                <div className="row">
-                    <h1 className={stylesContact.contactHeading}>Napiste nam</h1>
-                </div>
-                <div className="row justify-content-center">
-                    <div className={cx('col-md-5', stylesContact.contactFormWrapper)}>
-                        <form>
-                            <div className={stylesContact.formGroup}>
-                                <label htmlFor="inputName" className={stylesContact.formLabel}>
-                                    Your name
-                                    <input
-                                        type="name"
-                                        className={stylesContact.contactFormField}
-                                        id="inputName"
-                                        aria-describedby="nameHelp"
-                                        placeholder="Your name here"
-                                    />
-                                </label>
-                            </div>
-                            <div className={stylesContact.formGroup}>
-                                <label htmlFor="inputEmail" className={stylesContact.formLabel}>
-                                    Your email
-                                    <input
-                                        type="email"
-                                        className={stylesContact.contactFormField}
-                                        id="inputEmail"
-                                        placeholder="Your email here"
-                                    />
-                                </label>
-                            </div>
-                            <div className={stylesContact.formGroup}>
-                                <label htmlFor="inputSubject" className={stylesContact.formLabel}>
-                                    Subject
-                                    <input
-                                        type="email"
-                                        className={stylesContact.contactFormField}
-                                        id="inputSubject"
-                                        placeholder="Subject"
-                                    />
-                                </label>
-                            </div>
-                            <div className={stylesContact.formGroup}>
-                                <label htmlFor="textareaMessage" className={stylesContact.formLabel}>
-                                    Message
-                                    <textarea
-                                        className={stylesContact.contactFormField}
-                                        id="textareaMessage"
-                                        rows="3"
-                                        placeholder="Your message here"
-                                    />
-                                </label>
-                            </div>
-                            <BlockButton type="submit">Odeslat</BlockButton>
-                        </form>
+    //   Form validation state
+    const [errors, setErrors] = useState({});
+
+    //   Setting button text on form submission
+    const [buttonText, setButtonText] = useState('Send');
+
+    // Setting success or failure messages states
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showFailureMessage, setShowFailureMessage] = useState(false);
+
+    // Validation check method
+    const handleValidation = () => {
+        const tempErrors = {};
+        let isValid = true;
+
+        if (fullname.length <= 0) {
+            tempErrors.fullname = true;
+            isValid = false;
+        }
+        if (email.length <= 0) {
+            tempErrors.email = true;
+            isValid = false;
+        }
+        if (subject.length <= 0) {
+            tempErrors.subject = true;
+            isValid = false;
+        }
+        if (message.length <= 0) {
+            tempErrors.message = true;
+            isValid = false;
+        }
+
+        setErrors({ ...tempErrors });
+        console.log('errors', errors);
+        return isValid;
+    };
+
+    //   Handling form submit
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const isValidForm = handleValidation();
+
+        if (isValidForm) {
+            setButtonText('Sending');
+            const res = await fetch('/api/sendgrid', {
+                body: JSON.stringify({
+                    email,
+                    fullname,
+                    subject,
+                    message,
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'POST',
+            });
+
+            const { error } = await res.json();
+            if (error) {
+                console.log(error);
+                setShowSuccessMessage(false);
+                setShowFailureMessage(true);
+                setButtonText('Send');
+                return;
+            }
+            setShowSuccessMessage(true);
+            setShowFailureMessage(false);
+            setFullname('');
+            setEmail('');
+            setSubject('');
+            setMessage('');
+            setButtonText('Send');
+        }
+    };
+
+    return (
+        <>
+            <Head>
+                <title>Narativ | Kontakt</title>
+                <meta property="og:title" content="Narativ | Kontakt" key="title" />
+            </Head>
+
+            <FeaturedImage image={headerImage.data.image.url} />
+            <div className={stylesContact.contactBgBox}>
+                <div className="container pb-5 pt-2">
+                    <div className="row">
+                        <h1 className={stylesContact.contactHeading}>Napiste nam</h1>
+                    </div>
+                    <div className="row justify-content-center">
+                        <div className={cx('col-md-5', stylesContact.contactFormWrapper)}>
+                            <form onSubmit={handleSubmit}>
+                                <div className={stylesContact.formGroup}>
+                                    <label htmlFor="inputName" className={stylesContact.formLabel}>
+                                        Your name
+                                        <input
+                                            type="name"
+                                            className={stylesContact.contactFormField}
+                                            id="inputName"
+                                            value={fullname}
+                                            onChange={(e) => {
+                                                setFullname(e.target.value);
+                                            }}
+                                            name="fullname"
+                                            aria-describedby="nameHelp"
+                                            placeholder="Your name here"
+                                        />
+                                    </label>
+                                </div>
+                                <div className={stylesContact.formGroup}>
+                                    <label htmlFor="inputEmail" className={stylesContact.formLabel}>
+                                        Your email
+                                        <input
+                                            type="email"
+                                            className={stylesContact.contactFormField}
+                                            id="inputEmail"
+                                            name="email"
+                                            value={email}
+                                            onChange={(e) => {
+                                                setEmail(e.target.value);
+                                            }}
+                                            placeholder="Your email here"
+                                        />
+                                    </label>
+                                </div>
+                                <div className={stylesContact.formGroup}>
+                                    <label htmlFor="inputSubject" className={stylesContact.formLabel}>
+                                        Subject
+                                        <input
+                                            type="text"
+                                            className={stylesContact.contactFormField}
+                                            id="inputSubject"
+                                            name="subject"
+                                            value={subject}
+                                            onChange={(e) => {
+                                                setSubject(e.target.value);
+                                            }}
+                                            placeholder="Subject"
+                                        />
+                                    </label>
+                                </div>
+                                <div className={stylesContact.formGroup}>
+                                    <label htmlFor="textareaMessage" className={stylesContact.formLabel}>
+                                        Message
+                                        <textarea
+                                            className={stylesContact.contactFormField}
+                                            id="textareaMessage"
+                                            rows="3"
+                                            name="message"
+                                            value={message}
+                                            onChange={(e) => {
+                                                setMessage(e.target.value);
+                                            }}
+                                            placeholder="Your message here"
+                                        />
+                                    </label>
+                                </div>
+                                <BlockButton type="submit">{buttonText}</BlockButton>
+                                <div className="text-left">
+                                    {showSuccessMessage && (
+                                        <p className="text-green-500 font-semibold text-sm my-2">
+                                            Thank you! Your Message has been delivered.
+                                        </p>
+                                    )}
+                                    {showFailureMessage && (
+                                        <p className="text-red-500">Oops! Something went wrong, please try again.</p>
+                                    )}
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>
-);
+        </>
+    );
+};
 
 Contact.propTypes = {
     headerImage: PropTypes.shape({
